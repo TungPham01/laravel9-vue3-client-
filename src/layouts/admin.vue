202bca851fb4fb6c67b9fce23883115f61c7586f
Tung Pham <phamduytung11@gmail.com>
2022-11-30 17:38:36 +0700

    interigate bootstrap

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -1,14 +1,22 @@
 <template>
-  layout administration
-  <router-view></router-view>
+  <TheHeader />
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-3">Sider bar</div>
+      <div class="col-sm-9"><router-view></router-view></div>
+    </div>
+  </div>
 </template>
 
 <script>
+import TheHeader from "../components/TheHeader.vue";
 export default {
-
-}
+  components: {
+    TheHeader,
+  },
+};
 </script>
 
 <style>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import router from "./routers/index";
 import App from "./App.vue";
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import 'bootstrap/dist/css/bootstrap-utilities.min.css';
 
 const app = createApp(App);
 app.use(router);
